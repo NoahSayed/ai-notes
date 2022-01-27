ee81e3f7845e68c505a016683ac0273c49451ea0
Noah <noahsayeddb@gmail.com>
2022-01-27 23:43:22 +0530

    js updated

--- game.js
+++ game.js
@@ -2,8 +2,26 @@
 var buttonColors = ["red","green","blue","yellow"];
 var gamePattern = [];
 var userClickedPattern = [];
+var level = 0
+
+
+$(document).keypress(function(){
+    if(!started){
+         $("h1").text("level" + level)
+            nextSequence();
+                started==true;
+        }
+})
+
+//functions
 function playsound(name){
     var audio = new audio("sounds/" + name + ".mp3");
+}
+function animatepress(currentColor){
+    $("#" + currentColor).addclass("pressed")
+    setTimeout(function(){
+        $("#" + currentColor.removeclass("pressed"))
+    },100);
 }
 
 
@@ -11,6 +29,7 @@ $(".btn").click(function(){
 var userChosenColor = $(this).attr("id");
 userClickedPattern.push(userChosenColor);
 playsound(userChosenColor);
+animatepress(userChosenColor)
 })
 
     
@@ -18,6 +37,8 @@ playsound(userChosenColor);
 
 
 function nextSequence(){
+    level ++;
+    $("h1").text("level" + level);
 var randomNumber = Math.floor(Math.random() * 4);
 var randomcolorChosen = buttonColors[randomNumber];
 
