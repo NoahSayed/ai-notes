0f5146ce02ce302de2ba97682dd1c05805a9c8aa
Saad Evara <saadevara@gmail.com>
2022-02-11 07:18:12 +0530

    Add quote generator

--- Memory game/game.js
+++ Memory game/game.js
@@ -37,7 +37,7 @@ function checkAnswer(currentLevel) {
     } else {
       playSound("wrong");
       $("body").addClass("game-over");
-      $("#level-title").text("Game Over, Press Any Key to Restart");
+      $("#level-title").text("Game Over, press any key to restart.");
 
       setTimeout(function () {
         $("body").removeClass("game-over");
